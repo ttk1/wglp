90f271af71714dc2bddee71ee50d5fbfd25affeb
tama <tama@ttk1.net>
2019-06-18 02:49:58 +0900

    一旦保存

--- src/matrix.js
+++ src/matrix.js
@@ -7,3 +7,25 @@ exports.myclass = class myclass {
         console.log('hoge');
     }
 }
+
+exports.mat4 = class mat4 {
+    constructor() {
+        this.mat = [];
+        for (let i = 0; i < 4; i++) {
+            this.mat[i] = [];
+            for (let j = 0; j < 4; j++) {
+                this.mat[i][j] = 0;
+            }
+        }
+    }
+
+    flat() {
+        const arr = [];
+        for (let i = 0; i < 4; i++) {
+            for (let j = 0; j < 4; j++) {
+                arr.push(this.mat[i][j]);
+            }
+        }
+        return arr;
+    }
+}
