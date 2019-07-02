809fb9cdbce8d651f1b6f7b75cb1a6eaa0dd7c2c
tama <tama@ttk1.net>
2019-07-02 23:02:19 +0900

    色付けてみた。

--- src/glsl/test.frag.glsl
+++ src/glsl/test.frag.glsl
@@ -1,6 +1,7 @@
 #version 300 es
 precision mediump float;
+in vec4 hoge;
 out vec4 color;
 void main(void) {
-    color = vec4(0, 1.0, 0, 1.0);
+    color = hoge;
 }
--- src/glsl/test.vert.glsl
+++ src/glsl/test.vert.glsl
@@ -1,5 +1,6 @@
 #version 300 es
 in vec3 pos;
+out vec4 hoge;
 
 uniform mat4 rotX;
 uniform mat4 rotY;
@@ -8,4 +9,5 @@ uniform mat4 prj;
 
 void main(void) {
     gl_Position = prj * (rotX * rotY * rotZ) * vec4(pos, 1.0);
+    hoge = vec4(gl_Position.rgb * 3.0, 100.0);
 }
