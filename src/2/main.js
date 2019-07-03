a715a457dbb51ffa7025148d9136351fbbabe1e7
tama <tama@ttk1.net>
2019-07-04 03:53:36 +0900

    requestAnimationFrame使ってみた。

--- src/2/main.js
+++ src/2/main.js
@@ -6,11 +6,14 @@ exports.start = canvas => {
     const sp = require('./shaderProgram').getShaderProgram(gl);
     require('./attribute').setAttributes(gl, sp);
 
-    let rad = 0;
-    window.setInterval(() => {
+    const start_time = Date.now();
+    animate();
+
+    function animate() {
+        let rad = (Date.now() - start_time) / 6000;
         require('./uniform').setUniforms(gl, sp, 13 * rad, 17 * rad, 19 * rad);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
         gl.drawArrays(gl.TRIANGLES, 0, 3 * 12);
-        rad += 0.003;
-    }, 15);
+        window.requestAnimationFrame(animate);
+    }
 }
