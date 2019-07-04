1ff06267d8c73cecbf707c10b67e92f239568003
tama <tama@ttk1.net>
2019-07-04 23:15:48 +0900

    listをjsonから読み込むようにしてみた。

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 window.onload = () => {
+    const ul = document.getElementById('index');
     fetch('./index.json').then(
         response => response.json()
     ).then(
         index => {
             index.forEach(
-                entry => console.log(entry)
+                entry => appendEntry(entry)
             );
         }
     );
+    function appendEntry(entry) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = './work.html?id=' + entry.id;
+        a.innerHTML = entry.title;
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
 }
