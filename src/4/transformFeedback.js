04295997f7f5fa11ef7064e7639bae21f54c7317
tama <tama@ttk1.net>
2019-07-09 00:19:06 +0900

    chromeだと、DYNAMIC_COPYにするとエラーが出なくなる。

--- src/4/transformFeedback.js
+++ src/4/transformFeedback.js
@@ -3,7 +3,7 @@ exports.setTransformFeedback = gl => {
     const tf = gl.createTransformFeedback();
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 0.0, 0.0, 0.0]), gl.STREAM_READ);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([0.0, 0.0, 0.0, 0.0]), gl.DYNAMIC_COPY);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
     gl.bindTransformFeedback(gl.TRANSFORM_FEEDBACK, tf);
     gl.bindBufferBase(gl.TRANSFORM_FEEDBACK_BUFFER, 0, vbo);
