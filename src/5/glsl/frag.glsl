aa84820b200079b05c122eadae4c77e044912a45
tama <tama@ttk1.net>
2019-07-10 03:41:57 +0900

    幅をパラメータ化

--- src/5/glsl/frag.glsl
+++ src/5/glsl/frag.glsl
@@ -2,9 +2,12 @@
 precision mediump float;
 in vec3 color;
 out vec4 outColor;
+
+const float w = 20.0;
+
 void main(void) {
-    if (mod(gl_FragCoord.x, 20.0) <= 10.0 && mod(gl_FragCoord.y, 20.0) <= 10.0 ||
-        mod(gl_FragCoord.x, 20.0) > 10.0 && mod(gl_FragCoord.y, 20.0) > 10.0) {
+    if (mod(gl_FragCoord.x, w * 2.0) <= w && mod(gl_FragCoord.y, w * 2.0) <= w ||
+        mod(gl_FragCoord.x, w * 2.0) > w && mod(gl_FragCoord.y, w * 2.0) > w) {
         outColor = vec4(0,1,0,1);
     } else {
         outColor = vec4(0,0,1,1);
