61e55c03c09f484ea72f34a2a53a7e858f7706b7
tama <tama@ttk1.net>
2019-07-14 19:08:39 +0900

    [#7] 一旦保存

--- src/5/attribute.ts
+++ src/5/attribute.ts
@@ -2,14 +2,14 @@ exports.setAttributes = (gl: WebGLRenderingContext, sp: WebGLProgram) => {
     const idx: number = gl.getAttribLocation(sp, 'pos');
     gl.enableVertexAttribArray(idx);
 
-    const vertices : number[] = [
+    const vertices: number[] = [
         1.0, 1.0, 0.0,
         -1.0, 1.0, 0.0,
         1.0, -1.0, 0.0,
         -1.0, -1.0, 0.0,
     ];
 
-    const buf : WebGLBuffer = gl.createBuffer();
+    const buf: WebGLBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, buf);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
     gl.vertexAttribPointer(idx, 3, gl.FLOAT, false, 0, 0);
--- src/5/main.ts
+++ src/5/main.ts
@@ -1,9 +1,9 @@
-exports.start = (canvas : HTMLCanvasElement) => {
+exports.start = (canvas: HTMLCanvasElement): void => {
     canvas.width = 500;
     canvas.height = 500;
 
-    const gl : WebGLRenderingContext = require('./glContext').getContext(canvas);
-    const sp : WebGLProgram = require('./shaderProgram').getShaderProgram(gl);
+    const gl: WebGLRenderingContext = require('./glContext').getContext(canvas);
+    const sp: WebGLProgram = require('./shaderProgram').getShaderProgram(gl);
     require('./attribute').setAttributes(gl, sp);
 
     gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
