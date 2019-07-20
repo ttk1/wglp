9356b0e282b6caaccb91491a5062c21953842cff
tama <tama@ttk1.net>
2019-07-21 03:34:23 +0900

    点を四角から円に

--- src/6/glsl/frag.glsl
+++ src/6/glsl/frag.glsl
@@ -5,7 +5,12 @@ in vec3 color;
 out vec4 outColor;
 
 void main(void) {
-    outColor = vec4(color, 1.0);
+    vec2 d = gl_PointCoord * 2.0 - 1.0;
+    if (length(d) < 1.0) {
+        outColor = vec4(color, 1.0);
+    } else {
+        discard;
+    }
 }
 
 // gl_FragCoord
