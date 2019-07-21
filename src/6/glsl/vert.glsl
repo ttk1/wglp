cad173e4caafccee61220d954e18039642f621e5
tama <tama@ttk1.net>
2019-07-22 04:36:32 +0900

    パラメータ調整

--- src/6/glsl/vert.glsl
+++ src/6/glsl/vert.glsl
@@ -9,11 +9,12 @@ out vec2 next_vel;
 void main(void) {
     gl_Position = vec4(pos, 0.0, 1.0);
     gl_PointSize = 2.0;
-    next_pos = pos + vel * 0.01;
+    next_pos = pos + vel * 0.1;
 
-    // 空気抵抗、速度の二乗に比例
-    vec2 res = vel * pow(length(vel), 2.0) * 0.0005;
     // 重力加速度、マウス位置との距離の二乗に反比例
-    vec2 acc = (mouse - pos) / pow(distance(mouse, pos), 2.0);
-    next_vel = vel - res + acc;
+    vec2 acc = ((mouse - pos) / pow(distance(mouse, pos) + 1.0, 2.0)) * 0.01;
+    next_vel = vel + acc;
+    // 空気抵抗、速度の二乗に比例
+    vec2 res = next_vel * pow(length(next_vel), 2.0) * 0.05;
+    next_vel = next_vel - res;
 }
